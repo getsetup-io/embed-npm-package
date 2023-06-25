70a76c4033b2aaf18cf215c1080a32a6d9bb424f
Mitchell Lawson-Burton <mitchell@getsetup.io>
2023-06-26 11:31:24 +1200

    Only add tags to prereleases.

--- .github/workflows/setPackageVersion.js
+++ .github/workflows/setPackageVersion.js
@@ -2,19 +2,19 @@ const fs = require("fs");
 
 const packageJsonPath = './package.json';
 
-const packageJsonContent = JSON.parse(fs.readFileSync(packageJsonPath))
+const packageJsonContent = JSON.parse(fs.readFileSync(packageJsonPath));
 
-const branch = process.env.CI_BRANCH.replace(/\/|_/g, '-').replace(/^refs-heads-/, '');
+const gitRef = process.env.REF_NAME.replace(/\/|_/g, '-');
 const commitSha = process.env.COMMIT_SHA.substring(0, 7);
 const currentDate = new Date();
 const date = currentDate.toISOString().replace(/:/g, '');
 
-console.log(`branch: ${branch}`);
+console.log(`Git Ref: ${gitRef}`);
 console.log(`commitSha: ${commitSha}`);
 
-if(branch != 'main') {
-  // Don't add a tag if we are on main, just go with the plain version.
-  packageJsonContent.version = `${packageJsonContent.version}-${branch}-${date}-${commitSha}`;
+if (process.env.IS_PRERELEASE) {
+  // Only add a tag to the version if we are releasing a prerelease.
+  packageJsonContent.version = `${packageJsonContent.version}-${gitRef}-${date}-${commitSha}`;
 }
 console.log(`Package version: ${packageJsonContent.version}`);
 
@@ -22,5 +22,5 @@ fs.writeFileSync(packageJsonPath, JSON.stringify(packageJsonContent, null, 2), '
 
 // Also write a version file so we can report the version to Sentry.
 const versionJsonPath = './src/version.json';
-const versionJsonContent = { version: packageJsonContent.version}
+const versionJsonContent = { version: packageJsonContent.version };
 fs.writeFileSync(versionJsonPath, JSON.stringify(versionJsonContent, null, 2), 'utf8');
