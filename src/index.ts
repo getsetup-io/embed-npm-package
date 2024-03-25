08f395a9f8665f227d2aec296a72c60f607e559b
Mitchell Lawson-Burton <mitchell@getsetup.io>
2024-03-25 15:48:18 +1300

    Update URLs

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const targetPageUrls = {
   fitness: 'https://embed.getsetup.io/embedded/{embeddingOrgId}/fitness',
   joinClass: 'https://lobby-embed.getsetup.io/session/{sessionId}',
   // The below will be updated to final URLs later.
-  discover: 'https://embed-webapp.www.getsetup.io/discovery{partnerId}',
+  discover: 'https://embed-webapp.www.getsetup.io/discovery/{partnerId}',
   watch: 'https://embed-webapp.www.getsetup.io/watch/{classId}',
 }
 
